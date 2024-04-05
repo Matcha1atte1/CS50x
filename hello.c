359b92e475e243e155c60996b7299f62fcce9858
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-04-05 03:41:18 +0000

    Fri, Apr 5, 2024, 11:41 AM +08:00

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 int main(void)
 {
-    string answer = get_string  ("What's your name ?");
+    string answer = get_string  ("What's your name ? ");
     printf("hello, %s\n", answer);
 }
 
