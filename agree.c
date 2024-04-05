27604dee578af41053035572cb083b4e96696495
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-04-05 04:35:24 +0000

    Fri, Apr 5, 2024, 12:35 PM +08:00

--- agree.c
+++ agree.c
@@ -7,5 +7,12 @@ int main(void)
     char c = get_char ("Do you agree? ");
 
     //check whether agreed
-    if (c)
+    if (c == 'Y' || c == 'y')
+    {
+        printf ("Agreed.\n");
+    }
+    else if (c == 'N' || c == 'n')
+    {
+        printf ("Not agreed.\n");
+    }
 }
