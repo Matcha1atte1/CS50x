b99909e8fedf0cb854683665f2da60532dfc9ece
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-04-13 08:32:32 +0000

    Sat, Apr 13, 2024, 4:32 PM +08:00

--- mario.c
+++ mario.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
+    int n = get_int("Size: ");
+
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < 3; j++)
         {
