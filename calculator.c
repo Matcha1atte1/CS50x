8959c3fc5048e02e0eb784207f42ddf55a4f1342
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-04-13 04:12:28 +0000

    Sat, Apr 13, 2024, 12:12 PM +08:00

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+{
+    //prompt user for x
+    int x = get_int ("x: ");
+
+    //prompt user for y
+    int y = get_int ("y: ");
+
+    //perform addition
+    printf ("%i\n", x + y);
+}
