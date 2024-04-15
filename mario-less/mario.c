fef65cc76b951e166a19b9d5c3a7af744076ca4f
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-04-15 04:51:06 +0000

    Mon, Apr 15, 2024, 12:51 PM +08:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -15,7 +15,7 @@ int main (void)
     {
         for (int j = 0; j < n,j++)
         {
-            if (i + j < n - 1)
+            if (j)
             printf(" ");
             else
             printf("#");
