60fad8043a32f0be7f963448a58b8341b0d86539
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-04-15 04:55:26 +0000

    Mon, Apr 15, 2024, 12:55 PM +08:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -13,13 +13,13 @@ int main (void)
     //print n height pyramid
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n,j++)
+        for (int j = 0; j < n; j++)
         {
-            if (i + j< n - 1)
+            if (i + j < n - 1)
                 printf(" ");
             else
                 printf("#");
         }
         printf("\n");
     }
-
+}
