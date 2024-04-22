2673855a2d0a556ff38c85065a9bf240e712e222
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-04-22 02:45:37 +0000

    Mon, Apr 22, 2024, 10:45 AM +08:00

--- world/scores.c
+++ world/scores.c
@@ -1,1 +1,11 @@
 #include<stdio.h>
+int main(void)
+{
+    //scores
+    int score1 = 72;
+    int score2 = 73;
+    int score3 = 33;
+
+    //print average
+    printf("Average: %f\n", (score1 + score2 + score3)/3.0);
+}
