13e184f9d32248076b06137c3870bf97eed4cad6
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-04-23 03:03:50 +0000

    Tue, Apr 23, 2024, 11:03 AM +08:00

--- world/scores.c
+++ world/scores.c
@@ -4,7 +4,8 @@ int main(void)
 {
     // get scores
     int scores[3];
-    for()
+    for(i = 0; i < 3; i++)
+    scores[i] = 
 
     //print average
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2])/3.0);
