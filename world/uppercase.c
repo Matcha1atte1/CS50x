05389144b7d162302ef85db0a696157d76e56e27
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-06-11 07:16:05 +0000

    Tue, Jun 11, 2024, 3:16 PM +08:00

--- world/uppercase.c
+++ world/uppercase.c
@@ -9,7 +9,7 @@ int main(void)
     printf("After: ");
     for(int i = 0, n = strlen(s); i < n; i++)
     {
-        printf("%c\n", toupper(s[i]));
+        printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
