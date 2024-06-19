dd9ae3e4043c52a0e0a3aaf38d5ed9d368fb7f5f
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-06-19 05:19:46 +0000

    Wed, Jun 19, 2024, 1:19 PM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -15,8 +15,8 @@
         string word2 = get_string("Player 2: ");
 
         //compute the score of each word
-        int score1 = compute_score(word 1);
-        int score2 = compute_score(word 2);
+        int score1 = compute_score(word1);
+        int score2 = compute_score(word2);
 
     }
     int compute_score(string word);
@@ -25,6 +25,7 @@
         int score = 0
 
         //compute score for each character
+        for(int i = 0, length = strlen(word); )
 
 
     })
