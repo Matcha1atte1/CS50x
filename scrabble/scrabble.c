63d7ee2f571a1ea1b57973d0aab370025f74e851
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-06-19 04:39:18 +0000

    Wed, Jun 19, 2024, 12:39 PM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -4,6 +4,7 @@
     #include<string.h>
 
     int compute_score(string word);
+    
     int main(void)
     {
         //prompt the user for two words
@@ -11,5 +12,11 @@
         string word2 = get_string("Player 2: ");
 
         //compute the score of each word
-        int score1 = compute_score( string word)
+        int score1 = compute_score(word 1);
+        int score2 = compute_score(word 2);
+
     }
+    int compute_score(string word);
+    {
+        //compute and return score for word
+    })
