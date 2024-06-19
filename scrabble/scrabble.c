47aaf343bba6665ab62dceffa7f61926d9965e9b
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-06-19 04:17:58 +0000

    Wed, Jun 19, 2024, 12:17 PM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,4 +8,7 @@
         //prompt the user for two words
         string word1 = get_string("Player 1: ");
         string word2 = get_string("Player 2: ");
+
+        //compute the score of each word
+        
     }
