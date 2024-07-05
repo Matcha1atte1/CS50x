ea87437c05345dfa68f7e84efaf382202608ffed
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-07-05 05:35:48 +0000

    Fri, Jul 5, 2024, 1:35 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,8 @@
-#include<cs50.h>
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include<math.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
 
 int count_letters(string text);
 int count_words(string text);
@@ -10,26 +10,26 @@ int count_sentences(string text);
 
 int main(void)
 {
-    //Prompt the user for text
+    // Prompt the user for text
     string text = get_string("Text: ");
 
-    //Count the number of letters, words and sentences in the text
+    // Count the number of letters, words and sentences in the text
     float letters = count_letters(text);
     float words = count_words(text);
     float sentences = count_sentences(text);
 
-    //Compute the coleman-liau index
-    float L = (letters/words)*100;
-    float S = (sentences/words)*100;
-    int index = round(0.0588*L - 0.296*S - 15.8);
+    // Compute the coleman-liau index
+    float L = (letters / words) * 100;
+    float S = (sentences / words) * 100;
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
 
-    //Print the grade level
-    if(index >= 16)
+    // Print the grade level
+    if (index >= 16)
     {
         printf("Grade 16+\n");
     }
 
-    else if(index < 1)
+    else if (index < 1)
     {
         printf("Before Grade 1\n");
     }
@@ -42,11 +42,11 @@ int main(void)
 
 int count_letters(string text)
 {
-    //return the number of letters in the text
+    // return the number of letters in the text
     int countl = 0;
-    for(int i = 0, length = strlen(text); i < length; i++)
+    for (int i = 0, length = strlen(text); i < length; i++)
     {
-        if(isalpha(text[i]))
+        if (isalpha(text[i]))
         {
             countl++;
         }
@@ -56,12 +56,12 @@ int count_letters(string text)
 
 int count_words(string text)
 {
-    //return the number of words in the text
-    //number of words = spaces + 1
+    // return the number of words in the text
+    // number of words = spaces + 1
     int countw = 1;
-    for(int i = 0, length = strlen(text); i < length; i++)
+    for (int i = 0, length = strlen(text); i < length; i++)
     {
-        if(isspace(text[i]))
+        if (isspace(text[i]))
         {
             countw++;
         }
@@ -71,19 +71,19 @@ int count_words(string text)
 
 int count_sentences(string text)
 {
-    //return the number of sentences in the text
+    // return the number of sentences in the text
     int counts = 0;
-    for(int i = 0, length = strlen(text); i < length; i++)
+    for (int i = 0, length = strlen(text); i < length; i++)
     {
-        if(text[i] == '.')
+        if (text[i] == '.')
         {
             counts++;
         }
-        if(text[i] == '!')
+        if (text[i] == '!')
         {
             counts++;
         }
-        if(text[i] == '?')
+        if (text[i] == '?')
         {
             counts++;
         }
