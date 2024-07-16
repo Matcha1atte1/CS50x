cdeaa8dafc727cd6ae857a22118233b4db988308
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-07-16 06:48:52 +0000

    Tue, Jul 16, 2024, 2:48 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,5 +8,13 @@ int main(int argc, string argv[])
 {
     // Make sure program was run with just one command-line argument
 
-    //
+    // Make sure every character is argv[1] is a digit
+
+    // Convert argv[1] from a string to an int
+
+    // Prompt user for plaintext
+
+    // For each character in the plaintext:
+
+    // Rotate the character if it is a letter
 }
