2e8af022b76f41c1dede9e9f65d3a17b93fcf7d6
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-07-18 05:35:50 +0000

    Thu, Jul 18, 2024, 1:35 PM +08:00

---
+++ world/searchstring.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"}]
+
+    string s = get_string("String: ");
+    for(int i = 0; i < 6; i++)
+    {
+        if(strings[i] == s)
+        {
+            
+        }
+
+    }
+    }
+}
