12f05b12c3077c04af3783a5033122ec9f3cfd8a
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-07-18 06:33:51 +0000

    Thu, Jul 18, 2024, 2:33 PM +08:00

---
+++ world/phonebookperson.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
+int main(void)
+{
+    person people[3];
+
+    people[0].name
+}
