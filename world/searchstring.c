75a05f1b5df821843c3d5be134a05ce788199db1
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-07-18 05:43:02 +0000

    Thu, Jul 18, 2024, 1:43 PM +08:00

--- world/searchstring.c
+++ world/searchstring.c
@@ -11,9 +11,11 @@ int main(void)
     {
         if(strcmp(strings[i], s) == 0)
         {
-            
+            printf("Found\n");
+            return 0
         }
+    }
+    printf("Not found\n");
+    return 1
+ }
 
-    }
-    }
-}
