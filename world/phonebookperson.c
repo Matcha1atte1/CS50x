189afdb5518d65d38b446165665273bd0948e5cc
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-07-18 06:37:59 +0000

    Thu, Jul 18, 2024, 2:37 PM +08:00

--- world/phonebookperson.c
+++ world/phonebookperson.c
@@ -13,5 +13,24 @@ int main(void)
 {
     person people[3];
 
-    people[0].name
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-617-495-1000";
+
+    people[2].name = "John";
+    people[2].name = "+1-949-468-2750";
+
+    string name = get_string("Name: ");
+    for(int i = 0; i < 3; i++)
+    {
+        if(strcmp(people[i].name, name) == 0)
+        {
+            printf("Found %s\n", people[i].number);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
