e211bcf0687bab391175646b1a93851545889283
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-07-18 05:18:40 +0000

    Thu, Jul 18, 2024, 1:18 PM +08:00

--- world/search.c
+++ world/search.c
@@ -3,5 +3,14 @@
 
 int main(void)
 {
-    int numbers[] = { }
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+
+    int n = get_int("Number: ");
+    for(int i = 0; i < 7; i++)
+    {
+        if(numbers[i] == n)
+        {
+            printf("Found\n");
+        }
+    }
 }
