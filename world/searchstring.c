f5266b9ca72ba1071baed2128bbf9097d45ecfc4
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-07-18 05:44:35 +0000

    Thu, Jul 18, 2024, 1:44 PM +08:00

--- world/searchstring.c
+++ world/searchstring.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"}]
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 
     string s = get_string("String: ");
     for(int i = 0; i < 6; i++)
@@ -12,10 +12,10 @@ int main(void)
         if(strcmp(strings[i], s) == 0)
         {
             printf("Found\n");
-            return 0
+            return 0;
         }
     }
     printf("Not found\n");
-    return 1
+    return 1;
  }
 
