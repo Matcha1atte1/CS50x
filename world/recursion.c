f28fffbe11f1a9fac44808626893830119f2f16d
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-07-19 04:38:57 +0000

    Fri, Jul 19, 2024, 12:38 PM +08:00

--- world/recursion.c
+++ world/recursion.c
@@ -11,5 +11,12 @@ int main(void)
 
 void draw(int n)
 {
-    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < i + 1; j++
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
