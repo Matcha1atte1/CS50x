0743ad06383c8e352b9b2ded3c44e0e77df1d100
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-11 15:47:51 +0000

    Sun, Aug 11, 2024, 11:47 PM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -147,9 +147,10 @@ void tabulate(void)
         for(int j = 0; j < candidate_count; j++)
         {
             if(candidates[preferences[i][j]].eliminated == false)
-        {
+            {
             candidates[preferences[i][j]].votes++
-        }
+            break;
+            }
         }
     return;
 }
