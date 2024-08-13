3accf6f24001404cfaac31ff9b833c398036392f
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-13 05:19:11 +0000

    Tue, Aug 13, 2024, 1:19 PM +08:00

--- addresses.c
+++ addresses.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n;
-    printf("%i\n", *p);
+    string s = "HI!";
+    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
 }
