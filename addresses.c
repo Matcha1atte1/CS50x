0827dde5411daf40442ff5e4ebae36161aaf76c6
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-13 05:27:18 +0000

    Tue, Aug 13, 2024, 1:27 PM +08:00

--- addresses.c
+++ addresses.c
@@ -3,10 +3,6 @@
 
 int main(void)
 {
-    string s = "HI!";
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
+    char *s = "HI!";
+    printf("%s\n", s);
 }
