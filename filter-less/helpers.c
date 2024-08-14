cdb4bd4f791a8f7b87dc012a574308fd098e7838
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-14 08:15:17 +0000

    Wed, Aug 14, 2024, 4:15 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -24,6 +24,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    //Loop over all pixels
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            //Compute sepia values
+            sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+            sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+            sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+        }
+    }
     return;
 }
 
