69a703776f59c16a972262bb51718dc380dbb728
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-14 02:53:12 +0000

    Wed, Aug 14, 2024, 10:53 AM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -39,10 +39,11 @@ int main(int argc, char *argv[])
     fwrite(header, HEADER_SIZE, 1, ouput);
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    fread(&buffer, sizeof(int16_t), 1, input);
-    buffer *= factor;
-    fwrite(&buffer, sizeof(int16_t), 1, output);
-
+    while(fread(&buffer, sizeof(int16_t), 1, input) != 0)
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
     // Close files
     fclose(input);
     fclose(output);
