a36b1a6d3fc9827b23935b6b5b44d663d702cecb
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-14 07:49:19 +0000

    Wed, Aug 14, 2024, 3:49 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    //loop over all pixels
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        //take average of red, green and blue
+
+    }
+
+
+
+    //Update pixel values
     return;
 }
 
