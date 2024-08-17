92a63ebb4b3c94a7679b9e2e21981de863c94a51
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-17 09:56:01 +0000

    Sat, Aug 17, 2024, 5:56 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -30,9 +30,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for(int j = 0; j < width; j++)
         {
             //Compute sepia values
-            float sepiaRed = .393 * image[i][j].rbgtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
-            float sepiaGreen = .349 * image[i][j].rbgtRed + .686 * image[i][j].rgbtGreen + .168 *  image[i][j].rgbtBlue;
-            float sepiaBlue = .272 *image[i][j].rbgtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+            float sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            float sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 *  image[i][j].rgbtBlue;
+            float sepiaBlue = .272 *image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
 
             int SR = round(sepiaRed);
             int SG = round(sepiaGreen);
@@ -52,8 +52,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 
             //Update pixel with sepia values
             image[i][j].rgbtRed = SR;
-            image[i][j].rgbtgreen = SG;
-            image[i][j].rgbtblue = SB;
+            image[i][j].rgbtGreen = SG;
+            image[i][j].rgbtBlue = SB;
         }
     }
     return;
@@ -79,40 +79,40 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE copy[height][width]
+    RGBTRIPLE copy[height][width];
     for(int i = 0; i < height; i++)
     {
         for(int j = 0; j < width; j++)
         {
             copy[i][j] = image[i][j];
             //corner, 4 values
-            int sumcornerltred = copy[0][0].rgbtred + copy[0][1].rgbtred + copy[1][0].rgbtred + copy[1][1].rgbtred;
-            image[0][0].rgbtred = (sumcornerltred) / 4;
-            int sumcornerltgreen = copy[0][0].rgbtgreen + copy[0][1].rgbtgreen + copy[1][0].rgbtgreen + copy[1][1].rgbtgreen;
-            image[0][0].rgbtgreen = (sumcornerltgreen) / 4;
-            int sumcornerltblue = copy[0][0].rgbtblue + copy[0][1].rgbtblue + copy[1][0].rgbtblue + copy[1][1].rgbtblue;
-            image[0][0].rgbtblue = (sumcornerltblue) / 4;
+            int sumcornerltred = copy[0][0].rgbtRed + copy[0][1].rgbtRed + copy[1][0].rgbtRed + copy[1][1].rgbtRed;
+            image[0][0].rgbtRed = (sumcornerltred) / 4;
+            int sumcornerltgreen = copy[0][0].rgbtGreen + copy[0][1].rgbtGreen + copy[1][0].rgbtGreen + copy[1][1].rgbtGreen;
+            image[0][0].rgbtGreen = (sumcornerltgreen) / 4;
+            int sumcornerltblue = copy[0][0].rgbtBlue + copy[0][1].rgbtBlue + copy[1][0].rgbtBlue + copy[1][1].rgbtBlue;
+            image[0][0].rgbtBlue = (sumcornerltblue) / 4;
 
-            int sumcornerlbred = copy[height - 1][0].rgbtred + copy[height - 2][0].rgbtred + copy[height - 1][1].rgbtred + copy[height - 2][1].rgbtred;
-            image[height - 1][0].rgbtred = (sumcornerlbred) / 4;
-            int sumcornerlbgreen = copy[height - 1][0].rgbtgreen + copy[height - 2][0].rgbtgreen + copy[height - 1][1].rgbtgreen + copy[height - 2][1].rgbtgreen;
-            image[height - 1][0].rgbtgreen = (sumcornerlbgreen) / 4;
-            int sumcornerlbblue = copy[height - 1][0].rgbtblue + copy[height - 2][0].rgbtblue + copy[height - 1][1].rgbtblue + copy[height - 2][1].rgbtblue;
-            image[height - 1][0].rgbtblue = (sumcornerlbblue) / 4;
+            int sumcornerlbred = copy[height - 1][0].rgbtRed + copy[height - 2][0].rgbtRed + copy[height - 1][1].rgbtRed + copy[height - 2][1].rgbtRed;
+            image[height - 1][0].rgbtRed = (sumcornerlbred) / 4;
+            int sumcornerlbgreen = copy[height - 1][0].rgbtGreen + copy[height - 2][0].rgbtGreen + copy[height - 1][1].rgbtGreen + copy[height - 2][1].rgbtGreen;
+            image[height - 1][0].rgbtGreen = (sumcornerlbgreen) / 4;
+            int sumcornerlbblue = copy[height - 1][0].rgbtBlue + copy[height - 2][0].rgbtBlue + copy[height - 1][1].rgbtBlue + copy[height - 2][1].rgbtBlue;
+            image[height - 1][0].rgbtBlue = (sumcornerlbblue) / 4;
 
-            int sumcornerrtred = copy[0][width - 1].rgbtred + copy[0][width - 2].rgbtred + copy[1][width - 2].rgbtred + copy[1][width - 1].rgbtred;
-            image[0][width - 1].rgbtred = (sumcornerrtred) / 4;
-            int sumcornerrtgreen = copy[0][width - 1].rgbtgreen + copy[0][width - 2].rgbtgreen + copy[1][width - 2].rgbtgreen + copy[1][width - 1].rgbtgreen;
-            image[0][width - 1].rgbtgreen = (sumcornerrtgreen) / 4;
-            int sumcornerrtblue = copy[0][width - 1].rgbtblue + copy[0][width - 2].rgbtblue + copy[1][width - 2].rgbtblue + copy[1][width - 1].rgbtblue;
-            image[0][width - 1].rgbtblue = (sumcornerrtblue) / 4;
+            int sumcornerrtred = copy[0][width - 1].rgbtRed + copy[0][width - 2].rgbtRed + copy[1][width - 2].rgbtRed + copy[1][width - 1].rgbtRed;
+            image[0][width - 1].rgbtRed = (sumcornerrtred) / 4;
+            int sumcornerrtgreen = copy[0][width - 1].rgbtGreen + copy[0][width - 2].rgbtGreen + copy[1][width - 2].rgbtGreen + copy[1][width - 1].rgbtGreen;
+            image[0][width - 1].rgbtGreen = (sumcornerrtgreen) / 4;
+            int sumcornerrtblue = copy[0][width - 1].rgbtBlue + copy[0][width - 2].rgbtBlue + copy[1][width - 2].rgbtBlue + copy[1][width - 1].rgbtBlue;
+            image[0][width - 1].rgbtBlue = (sumcornerrtblue) / 4;
 
-            int sumcornerrbred = copy[height - 1][width - 1].rgbtred + copy[height - 1][width - 2].rgbtred + copy[height - 2][width - 2].rgbtred + copy[height - 2][width - 1].rgbtred;
-            image[height - 1][width - 1].rgbtred = (sumcornerrbred) / 4;
-            int sumcornerrbgreen = copy[height - 1][width - 1].rgbtgreen + copy[height - 1][width - 2].rgbtgreen + copy[height - 2][width - 2].rgbtgreen + copy[height - 2][width - 1].rgbtgreen;
-            image[height - 1][width - 1].rgbtgreen = (sumcornerrbgreen) /4;
-            int sumcornerrbblue = copy[height - 1][width - 1].rgbtblue + copy[height - 1][width - 2].rgbtblue + copy[height - 2][width - 2].rgbtblue + copy[height - 2][width - 1].rgbtblue;
-            image[height - 1][width - 1].rgbtblue = (sumcornerrbblue) /4;
+            int sumcornerrbred = copy[height - 1][width - 1].rgbtRed + copy[height - 1][width - 2].rgbtRed + copy[height - 2][width - 2].rgbtRed + copy[height - 2][width - 1].rgbtRed;
+            image[height - 1][width - 1].rgbtRed = (sumcornerrbred) / 4;
+            int sumcornerrbgreen = copy[height - 1][width - 1].rgbtGreen + copy[height - 1][width - 2].rgbtGreen + copy[height - 2][width - 2].rgbtGreen + copy[height - 2][width - 1].rgbtGreen;
+            image[height - 1][width - 1].rgbtGreen = (sumcornerrbgreen) /4;
+            int sumcornerrbblue = copy[height - 1][width - 1].rgbtBlue + copy[height - 1][width - 2].rgbtBlue + copy[height - 2][width - 2].rgbtBlue + copy[height - 2][width - 1].rgbtBlue;
+            image[height - 1][width - 1].rgbtBlue = (sumcornerrbblue) /4;
         }
     }
 
@@ -121,81 +121,81 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             //first row
             for(int j = 1; j < width - 1; j++)
             {
-                sumr = 0;
-                sumg = 0;
-                sumb = 0;
+                int sumr = 0;
+                int sumg = 0;
+                int sumb = 0;
                 for(int i = 0; i <= 1; i++)
                 {
                     for(int x = j - 1; x <= j + 1; x++)
                     {
-                        sumr += copy[i][x].rgbtred;
-                        sumg += copy[i][x].rgbtgreen;
-                        sumb += copy[i][x].rgbtblue;
+                        sumr += copy[i][x].rgbtRed;
+                        sumg += copy[i][x].rgbtGreen;
+                        sumb += copy[i][x].rgbtBlue;
                     }
                 }
-                image[0][j].rgbtred = sumr / 6;
-                image[0][j].rgbtgreen = sumg / 6;
-                image[0][j].rgbtblue = sumb / 6;
+                image[0][j].rgbtRed = sumr / 6;
+                image[0][j].rgbtGreen = sumg / 6;
+                image[0][j].rgbtBlue = sumb / 6;
             }
 
             //last row
             for(int j = 1; j < width - 1; j++)
             {
-                sumr = 0;
-                sumg = 0;
-                sumb = 0;
+                int sumr = 0;
+                int sumg = 0;
+                int sumb = 0;
                 for(int i = height - 2; i <= height - 1; i++)
                 {
                     for(int x = j - 1; x <= j + 1; x++)
                     {
-                        sumr += copy[i][x].rgbtred;
-                        sumg += copy[i][x].rgbtgreen;
-                        sumb += copy[i][x].rgbtblue;
+                        sumr += copy[i][x].rgbtRed;
+                        sumg += copy[i][x].rgbtGreen;
+                        sumb += copy[i][x].rgbtBlue;
                     }
                 }
-                image[height - 1][j].rgbtred = sumr / 6;
-                image[height - 1][j].rgbtgreen = sumg / 6;
-                image[height - 1][j].rgbtblue = sumb / 6;
+                image[height - 1][j].rgbtRed = sumr / 6;
+                image[height - 1][j].rgbtGreen = sumg / 6;
+                image[height - 1][j].rgbtBlue = sumb / 6;
             }
 
             //first column
             for(int i = 1; i <= height - 2; i++)
             {
-                sumr = 0;
-                sumg = 0;
-                sumb = 0;
+                int sumr = 0;
+                int sumg = 0;
+                int sumb = 0;
                 for(int j = 0; j <= 1; j++)
                 {
                     for(int x = i - 1; x <= i + 1; x++)
                     {
-                        sumr += copy[x][j].rgbtred;
-                        sumg += copy[x][j].rgbtgreen;
-                        sumb += copy[x][j].rgbtblue;
+                        sumr += copy[x][j].rgbtRed;
+                        sumg += copy[x][j].rgbtGreen;
+                        sumb += copy[x][j].rgbtBlue;
                     }
                 }
-                image[i][0].rgbtred = sumr / 6;
-                image[i][0].rgbtgreen = sumg / 6;
-                image[i][0].rgbtblue = sumb / 6;
+                image[i][0].rgbtRed = sumr / 6;
+                image[i][0].rgbtGreen = sumg / 6;
+                image[i][0].rgbtBlue = sumb / 6;
             }
 
             //last column
             for(int i = 1; i <= height - 2; i++)
             {
-                sumr = 0;
-                sumg = 0;
-                sumb = 0;
+                int sumr = 0;
+                int sumg = 0;
+                int sumb = 0;
                 for(int j = width - 2; j <= width - 1; j++)
                 {
                     for(int x = i - 1; x <= i + 1; x++)
                     {
-                        sumr += copy[x][j].rgbtred;
-                        sumg += copy[x][j].rgbtgreen;
-                        sumb += copy[x][j].rgbtblue;
+                        sumr += copy[x][j].rgbtRed;
+                        sumg += copy[x][j].rgbtGreen;
+                        sumb += copy[x][j].rgbtBlue;
                     }
                 }
-                image[i][width - 1].rgbtred = sumr / 6;
-                image[i][width - 1].rgbtgreen = sumg / 6;
-                image[i][width - 1].rgbtblue = sumb / 6;
+                image[i][width - 1].rgbtRed = sumr / 6;
+                image[i][width - 1].rgbtGreen = sumg / 6;
+                image[i][width - 1].rgbtBlue = sumb / 6;
             }
 
 
