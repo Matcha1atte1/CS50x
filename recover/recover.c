c51556ee6a62df03febe3a885b16a87c9567ea8b
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-18 06:53:14 +0000

    Sun, Aug 18, 2024, 2:53 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,31 +24,45 @@ int main(int argc, char *argv[])
     // Create a buffer for a block of data
     uint8_t buffer[512];
 
+    // set jpeg counter
+    int count = 0;
+
+    // set currenfile
+    FILE *currentfile = NULL;
+
+    // allocate memory for filename
+    char *filename = malloc(8);
+
     // while there is still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
-        int count = 0;
         // check if it is a jpeg header (start of a jpeg)
         if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] && 0xf0) == 0xe0)
         {
-            count++;
+            // create JPEGs from the data
+
             // check if it is the first jpeg found
-            if(count == 1)
+            if(count == 0)
             {
-                char *filename = malloc(8);
                 sprintf(filename, "%03i.jpg", 0);
                 FILE *currentfile = fopen(filename, "w");
+                count++;
             }
+            // else if already found a jpeg
             else
             {
-                fclose()
+                fclose(currentfile);
+                sprintf(filename, "%03i.jpg", count);
+                FILE *currentfile = fopen(filename, "w");
+                count++;
             }
-
-            // else if already found a jpeg
         }
-        // create JPEGs from the data
-
+        if(currentfile != NULL)
+        {
+            fwrite(buffer, sizeof(uint8_t), 512, currentfile);
+        }
     }
 
-
+    free(filename);
+    fclose(currentfile);
 }
