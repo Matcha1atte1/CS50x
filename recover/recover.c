08c9d7d46ea7b61e402f127e585ab43ffe58e874
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-18 05:10:30 +0000

    Sun, Aug 18, 2024, 1:10 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
         // check if it is a jpeg header
         if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] && 0xf0) == 0xe0)
         {
-            count++
+            count++;
             // check if it is the first jpeg found
             if(count == 1)
             {
