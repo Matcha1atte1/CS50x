99cbc661e31f5d23d02929d09c48c7f182be542f
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-18 06:33:57 +0000

    Sun, Aug 18, 2024, 2:33 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
             // check if it is the first jpeg found
             if(count == 1)
             {
-                char *jpeg0 = malloc(8);
+                char * = malloc(8);
                 sprintf(jpeg0, "%03i.jpg", 0);
                 FILE *output = fopen(jpeg0, "w");
                 fread(card, 1, 8, jpeg0);
