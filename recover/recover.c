a9c66ba385bc1e09cb6dbc6abbc03296f8a34e19
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-18 05:04:37 +0000

    Sun, Aug 18, 2024, 1:04 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -27,8 +27,13 @@ int main(int argc, char *argv[])
     // while there is still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
+        // check if it is a jpeg header
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] && 0xf0) == 0xe0)
+        {
+
+        }
         // create JPEGs from the data
-        
+
     }
 
 
