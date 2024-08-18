0937b12dcb6f6f2b558aa17965210d5188bea0bb
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-18 04:23:16 +0000

    Sun, Aug 18, 2024, 12:23 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // accept a single command-line argument
+    if(argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    // open the memory card
+    FILE *card = fopen(argv[1], "r");
+
+    // check file was opened properly
+    
+
 }
