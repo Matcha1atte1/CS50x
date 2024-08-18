7ed6f31ce995b8b1e5fb08a2b6717edc58a59ab5
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-18 04:38:14 +0000

    Sun, Aug 18, 2024, 12:38 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,7 +25,11 @@ int main(int argc, char *argv[])
     uint8_t buffer[512];
 
     // while there is still data left to read from the memory card
-    while (fread(buffer, 1, 512, ))
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        // create JPEGs from the data
+        
+    }
 
 
 }
