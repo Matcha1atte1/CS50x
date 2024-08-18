e2281a62ab63e0444ac5053a9ca3d2d51d7310a2
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-18 05:17:27 +0000

    Sun, Aug 18, 2024, 1:17 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
             // check if it is the first jpeg found
             if(count == 1)
             {
-                int sprintf 
+                int *sprintf = malloc(521);
                 sprintf(output, "%03i.jpg", 0);
             }
 
