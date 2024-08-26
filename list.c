ca82b6c77edcdcc1b7300fb2de7d9f61df381e9c
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-26 12:06:01 +0000

    Mon, Aug 26, 2024, 8:06 PM +08:00

--- list.c
+++ list.c
@@ -16,7 +16,26 @@ int main(void)
     int *tmp = malloc(4 * sizeof(int));
     if(tmp == NULL)
     {
-        
+        free(list);
         return 1;
     }
+
+    for(int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+
+    tmp[3] = 4;
+
+    free(list);
+
+    list = tmp;
+
+    for(int i = 0; i < 4; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+
+    free(list);
+    return 0;
 }
