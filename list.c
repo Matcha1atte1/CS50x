c0cea508055694e243ee4c36cb5d1bd3bd5724e1
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-08-27 11:37:31 +0000

    Tue, Aug 27, 2024, 7:37 PM +08:00

--- list.c
+++ list.c
@@ -13,10 +13,18 @@ int main(int argc, char *argv[])
 
     for(int i = 1; i < argc; i++)
     {
-        printf("%s\n", argv[i]);
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+        if(n == NULL)
+        {
+            return 1;
+        }
+        n->number = number;
+
     }
 
-    
+
 }
 
 
