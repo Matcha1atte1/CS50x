2f3c747e9ab18ea593a8b2419b7a5bff6a6cdf92
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-09-23 12:40:45 +0000

    Mon, Sep 23, 2024, 8:40 PM +08:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,5 +1,16 @@
 def calculate_quarters(dollars)
-    
+    quarters = 0
+    while True:
+        quarters += 1
+        dollars = dollars - 0.25
+        if dollars < 0.25:
+            break
+    return quarters
+
+
 def calculate_dimes(dollars)
 def calculate_nickels(dollars)
 def calculate_pennies(dollars)
+
+def main()
+
