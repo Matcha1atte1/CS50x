74882e706b5807808c93129d74c98bd26a58dde2
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-09-23 12:08:41 +0000

    Mon, Sep 23, 2024, 8:08 PM +08:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -3,6 +3,9 @@ while True:
             height = int(input("Height: "))
         except ValueError:
 
+         if 1 <= height <= 8:
+            break
+
 for row in range(height):
     for space in range(height - row - 1):
         print(" ", end="")
