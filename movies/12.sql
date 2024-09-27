3fb79f606d840d0b56552a58ebbad69b01d91a23
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-09-27 08:14:36 +0000

    Fri, Sep 27, 2024, 4:14 PM +08:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,3 +1,13 @@
 SELECT title
 FROM movies
-WHERE 
+WHERE id IN
+(
+    SELECT movie_id
+    FROM stars
+    WHERE person_id IN
+    (
+        SELECT id
+        FROM people
+        WHERE name LIKE '%Jennifer Lawrence%' AND '%Bradley Cooper%'
+    )
+);
