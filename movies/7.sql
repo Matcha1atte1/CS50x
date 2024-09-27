bbf31205b153164a0a176583ba30c84f71e720d9
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-09-27 07:29:00 +0000

    Fri, Sep 27, 2024, 3:29 PM +08:00

--- movies/7.sql
+++ movies/7.sql
@@ -2,4 +2,4 @@ SELECT movies.title, ratings.rating
 FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
 WHERE year = 2010
-ORDER BY ratings.rating DESC, ;
+ORDER BY ratings.rating DESC, title;
