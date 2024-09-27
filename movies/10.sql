f80448eacceb52d9b6db99df84655c6d651a9951
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-09-27 07:09:49 +0000

    Fri, Sep 27, 2024, 3:09 PM +08:00

---
+++ movies/10.sql
@@ -0,0 +1,13 @@
+SELECT name
+FROM people
+WHERE id IN
+(
+    SELECT person_id
+    FROM stars
+    WHERE movie_id =
+    (
+        SELECT movie_id
+        FROM ratings
+        WHERE rating >= 9.0
+    )
+);
