9df7912b1823e1fc340491e249d2f367d1ab8130
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-09-29 13:28:45 +0000

    Sun, Sep 29, 2024, 9:28 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- crime_scene_reports table to look for a crime scene report that matches the date and location of the crime
+SELECT id
+FROM crime_scene_reports
+WHERE year = 2023
+AND month = 7
+AND day = 28
+AND street = Humphrey;
