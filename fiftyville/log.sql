24e26ffbaf77212a1cbd5552650f50d8d537c9aa
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-05 09:34:26 +0000

    Sat, Oct 5, 2024, 5:34 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -17,7 +17,7 @@ WHERE year = 2023
 AND month = 7
 AND day = 28;
 
--- within 10 minutes of theft, thief got into a car at bakery parking lot, thief withdrew money at the ATM on Leggett Street, thief left the bakery and called someone and talked less than a minute,
+-- within 10 minutes of theft, thief got into a car at bakery parking lot, thief withdrew money at the ATM on Leggett Street earlier in the morning, thief left the bakery and called someone and talked less than a minute,
    plan to take the earliest flight out of fiftyville tomorrow, ask the person on the phone to buy ticket
 
 -- bakery_security_logs table
@@ -28,6 +28,16 @@ AND month = 7
 AND day = 28
 AND hour = 10
 AND(minute BETWEEN 15 AND 25);
+
+-- atm_transactions table
+SELECT account_number
+FROM atm_transactions
+WHERE year = 2023
+AND month = 7
+AND day = 28
+AND atm_location = 'Leggett Street'
+AND transaction_type = 'withdraw';
+
 
 
 
