a872c39d24c2cd9015a7d22702d886dcb488ad09
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-05 10:04:58 +0000

    Sat, Oct 5, 2024, 6:04 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -64,7 +64,30 @@ WHERE id = 4;
 SELECT *
 FROM bank_accounts
 JOIN people ON person_id.bank_accounts = id.people
-WHERE
+WHERE id IN
+(
+    SELECT id
+FROM people
+WHERE phone_number IN
+(
+    SELECT caller
+    FROM phone_calls
+    WHERE year = 2023
+    AND month = 7
+    AND day = 28
+    AND duration < 60
+)
+AND license_plate IN
+(
+    SELECT license_plate
+    FROM bakery_security_logs
+    WHERE year = 2023
+    AND month = 7
+    AND day = 28
+    AND hour = 10
+    AND(minute BETWEEN 15 AND 25)
+)
+);
 
 
 -- people table to find thief
