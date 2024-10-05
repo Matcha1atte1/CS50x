6d36cfac3375957e3ce1d19f4ad845fa51120704
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-05 09:15:34 +0000

    Sat, Oct 5, 2024, 5:15 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,11 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- crime_scene_reports table to look for a crime scene report that matches the date and location of the crime
-SELECT id
+SELECT *
 FROM crime_scene_reports
 WHERE year = 2023
 AND month = 7
 AND day = 28
 AND street = 'Humphrey Street';
 
--- output id 295,297
+
