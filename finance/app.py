ce281e84ad2db889ef22e9869b95920554950f0e
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-17 09:42:52 +0000

    Thu, Oct 17, 2024, 5:42 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -107,7 +107,7 @@ def buy():
         else:
             # record the purchase
             db.execute("INSERT INTO purchases (user_id, symbol, shares, price) VALUES (?, ?, ?, ?)", user_id, symbol, shares, quoted_data["price"])
-            db.execute("INSERT INTO history (user_id, symbol, shares, price))
+            db.execute("INSERT INTO history (user_id, symbol, shares, price, action) VALUES (?, ?, ?, ?, ?)", user_id, symbol, shares, price, "buy")
 
             # update the cash balance of the user
             db.execute("UPDATE users SET cash = cash - ? WHERE id = ?", total_cost, user_id)
@@ -123,6 +123,7 @@ def buy():
 def history():
     """Show history of transactions"""
     user_id = session.get("user_id")
+    
 
     return apology("TODO")
 
@@ -259,6 +260,7 @@ def sell():
 
         # proceed with sale
         db.execute("UPDATE purchases SET shares = shares - ? WHERE user_id=? AND symbol = ?", shares, user_id, symbol)
+        db.execute("INSERT INTO history (user_id, symbol, shares, price, action) VALUES (?, ?, ?, ?, ?)", user_id, symbol, shares, price, "sell")
 
         return redirect("/")
 
