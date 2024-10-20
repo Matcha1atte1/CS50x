9f8d0ac7d23002d05bfb584c314a763406efc2eb
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-20 07:54:57 +0000

    Sun, Oct 20, 2024, 3:54 PM +08:00

--- final/blackjack.py
+++ final/blackjack.py
@@ -14,3 +14,11 @@ pygame.display.set_caption('Blackjack Game')
 # set colour of blackjack table
 background_colour = (0, 128, 0)
 
+# main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
