af4ddbbbc0056e707b3776fac67a9f1fb1b5813f
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-20 07:44:12 +0000

    Sun, Oct 20, 2024, 3:44 PM +08:00

--- final/blackjack.py
+++ final/blackjack.py
@@ -6,5 +6,8 @@ pygame.init()
 
 # window dimensions
 screen_width, screen_height = 800, 600
-screen = 
+screen = pygame.display.set_mode(screen_width, screen_height)
+
+# window title
+pygame.display.set_caption('Blackjack Game')
 
