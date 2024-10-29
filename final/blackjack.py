aba46b064de4453eb7625d2f66a6ef915da64ac1
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-29 12:38:24 +0000

    Tue, Oct 29, 2024, 8:38 PM +08:00

--- final/blackjack.py
+++ final/blackjack.py
@@ -7,4 +7,6 @@ values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 # create deck of 52 cards sorted in order
 deck = [f"{value} of {suit}" for suit in suits for value in values]
 
-
+# function to shuffle deck
+def shuffle_deck(deck):
+    random.shuffle(deck)
