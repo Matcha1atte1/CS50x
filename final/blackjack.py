671adf76d4ef7a2fed884ab6f1ccd1c6c2604fa5
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-29 12:35:22 +0000

    Tue, Oct 29, 2024, 8:35 PM +08:00

--- final/blackjack.py
+++ final/blackjack.py
@@ -4,6 +4,7 @@ import random
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-# create deck of 52 cards
+# create deck of 52 cards sorted in order
 deck = [f"{value} of {suit}" for suit in suits for value in values]
+
 
