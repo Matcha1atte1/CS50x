36cd36f22a30e0a02f8b1380023094d49eec15ea
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-29 12:33:53 +0000

    Tue, Oct 29, 2024, 8:33 PM +08:00

--- final/blackjack.py
+++ final/blackjack.py
@@ -5,5 +5,5 @@ suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 # create deck of 52 cards
-deck = []
+deck = [f"{value} of {suit}" for suit in suits for value in values]
 
