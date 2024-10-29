6babe267d6584e756523dbf9df96e3bd1d3297e9
Starl1ght1 <165799286+Starl1ght1@users.noreply.github.com>
2024-10-29 12:30:52 +0000

    Tue, Oct 29, 2024, 8:30 PM +08:00

--- final/blackjack.py
+++ final/blackjack.py
@@ -4,4 +4,6 @@ import random
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
+# create deck of 52 cards
+deck = []
 
