8f38f525c8839ac8ca2ed84046d6aaab3001a86f
SHEN WEN'NA <165799286+Starl1ght1@users.noreply.github.com>
2024-12-11 08:20:59 +0000

    Wed, Dec 11, 2024, 4:20 PM +08:00

--- meal/meal.py
+++ meal/meal.py
@@ -4,16 +4,20 @@ time = input("What time is it?")
 hours = convert(time)
 if 7 < hours < 8:
     print("breakfast time")
-if 12 < hours < 13:
+elif 12 < hours < 13:
     print("lunch time")
-if 18 < hours < 19:
+elif 18 < hours < 19:
     print("dinner time")
+else:
+    print("not meal time")
 
 
 
 def convert(time):
     hours, minutes = time.split(":")
-    return float(hours + (minutes/60))
+    hours = float(hours)
+    minutes = float(minutes)
+    return hours + (minutes/60)
 
 if __name__ == "__main__":
     main()
