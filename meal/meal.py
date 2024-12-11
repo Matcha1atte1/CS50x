547f5792059d013d9117195510789e16f066d566
SHEN WEN'NA <165799286+Starl1ght1@users.noreply.github.com>
2024-12-11 08:26:26 +0000

    Wed, Dec 11, 2024, 4:26 PM +08:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,15 +1,15 @@
 def main():
 
-time = input("What time is it?")
-hours = convert(time)
-if 7 <= hours < 8:
-    print("breakfast time")
-elif 12 <= hours < 13:
-    print("lunch time")
-elif 18 <= hours < 19:
-    print("dinner time")
-else:
-    print("not meal time")
+    time = input("What time is it?")
+    hours = convert(time)
+    if 7 <= hours < 8:
+        print("breakfast time")
+    elif 12 <= hours < 13:
+        print("lunch time")
+    elif 18 <= hours < 19:
+        print("dinner time")
+    else:
+        print("not meal time")
 
 
 
@@ -21,4 +21,4 @@ def convert(time):
 
 if __name__ == "__main__":
     main()
-``
+
